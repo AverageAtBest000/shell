2ea68565fd0aeee45f6c35ff516a1493fdf83c9a
averageatbest <octaviogamez466@gmail.com>
2026-09-04 23:55:15 -0500

    feat: use waitpid() instead of wait

--- src/executor.c
+++ src/executor.c
@@ -19,7 +19,7 @@ void execute(char*** argv, char** cmd, int* argc)
         int val = execvp((*argv)[0], *argv);
         if(val == -1) perror("Command fail");
     }else{  
-        wait(NULL);
+        wait_pid(NULL);
     }
     
     reset(argc, cmd, argv);
