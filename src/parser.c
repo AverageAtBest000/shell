9a094d27a25b345aa21246b37b63d18eb2d8d0a8
averageatbest <octaviogamez466@gmail.com>
2026-09-17 16:22:10 -0500

    refactor: use function to append to cur_tok

--- src/parser.c
+++ src/parser.c
@@ -35,15 +35,10 @@ int get_tokens(int* argc, char*** argv, char** cmd, char delim){
                 continue;
             }
             
+
             if(cmd[i] != delim || in_string){
                 
-                char* temp = realloc( cur_tok, len(cur_tok) + 2 )
-
-                temp[len(cur_tok)] = cmd[i];
-                temp[len(cur_tok) + 1] = '\0';
-                
-                if(temp != NULL) cur_tok = temp;
-                else return -1;
+                if(append_to_cur_tok(&cur_tok, cmd[i]) == -1) return -1;
             }
             
         }
@@ -57,7 +52,7 @@ int get_tokens(int* argc, char*** argv, char** cmd, char delim){
 }
 
 
-void append_to_vector(char*** argv, char** cur_tok){
+int append_to_vector(char*** argv, char** cur_tok, char** temp){
 
     temp = realloc( argv, sizeof(argv) + sizeof(char*) );
     if(temp == NULL) return -1; 
@@ -67,5 +62,18 @@ void append_to_vector(char*** argv, char** cur_tok){
     (*argc)++;
     cur_tok[0] = '\0';
 
+
+}
+
+
+int append_to_cur_tok(char** cur_tok, char to_append){
+
+    char* temp = realloc( *cur_tok, len(cur_tok) + 2 )
+
+    temp[len(cur_tok)] = to_append;
+    temp[len(cur_tok) + 1] = '\0';
+    
+    if(temp != NULL) cur_tok = temp;
+    else return -1;
 
 }
