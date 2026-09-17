e5656f3bad72fa4d7f6ad00304d8bda29c02f27d
averageatbest <octaviogamez466@gmail.com>
2026-09-17 14:20:06 -0500

    feat: add first version of updated parser

--- src/parser.c
+++ src/parser.c
@@ -7,15 +7,7 @@ void tokenize(int* argc, char*** argv, char** cmd, char* delim)
 
     char* cmd_cpy = malloc(sizeof(char) * strlen(*cmd) + 1);
 
-    strcpy(cmd_cpy , *cmd );
-    
-    token = strtok(*cmd, delim);
-
-    for(int i = 0 ; token ; i++)
-    {
-        (*argv)[i] = token;
-        token = strtok(NULL, delim);
-    }
+    count_tokens(argc, cmd, delim);
 
     (*argv)[*argc] = NULL;
 
@@ -23,7 +15,43 @@ void tokenize(int* argc, char*** argv, char** cmd, char* delim)
 }
 
 
-int count_tokens(int* argc, char* cmd){
+void get_tokens(int* argc, char*** argv, char* cmd){
+
+        *argv = malloc((*argc + 1) * sizeof(char*));
+
+        char* cur_tok = malloc(1);
+        cur_tok[0] = '\0';
+
+        bool in_string = false;
+        int j = 0; 
+
+        for(int i = 0; i < strlen(cmd); i++){
+
+            if(cmd[i] == '"') in_string = !in_string;
+
+            if( i == strlen(cmd) && !(cmd[i] == ' ') || (cmd[i] == ' ' && len(cur_tok) == 0 && !in_string) ){
+                (*argv)[j++] = cur_tok;
+                cur_tok[0] = '\0' ;
+                continue;
+            }
+
+            if(cmd[i] != ' ' || in_string){
+                
+                char* temp = realloc( cur_tok, len(cur_tok) + 2 )
+                temp[len(cur_tok)] = cmd[i];
+                temp[len(cur_tok) + 1] = '\0';
+                
+                if(temp != NULL) cur_tok = temp;
+            }
+
+        }
+
+        free(cur_tok);
+
+}
+
+
+int count_tokens(int* argc, char* cmd, char* delim){
 
 
     bool in_string = false; 
@@ -32,7 +60,7 @@ int count_tokens(int* argc, char* cmd){
 
         if(strcmp(cmd[i], '"') && !in_string) in_string = !in_string;
         
-        if(strcmp(cmd[i], ' ') && !in_string) num_tok++;
+        if(strcmp(cmd[i], *delim) && !in_string) num_tok++;
     }
     return num_tok;
 }
