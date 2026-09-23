2699df2bde5ffe7445a4ce81f9025c17faae479c
averageatbest <octaviogamez466@gmail.com>
2026-09-22 23:11:11 -0500

    feat: use pragma once in header files

--- include/builtins.h
+++ include/builtins.h
@@ -1,8 +1,6 @@
-#ifndef BUILTINS_H
-#define BUILTINS_H
+#pragma once
 
 bool handleBuiltins(char** argv, int argc);
 void cd(char** argv, int argc);
 bool is_builtin(char* cmd);
 
-#endif
--- include/parser.h
+++ include/parser.h
@@ -1,7 +1,5 @@
-#ifndef PARSER_H
-#define PARSER_H
+#pragma once  
 
 void tokenize(int* argc, char*** argv, char** cmd, char delim);
 int get_tokens(int* argc, char*** argv, char** cmd, char delim);
-
-#endif
+  
